7f0e4d3a95adce5da7f2d23aba36b128a1051337
Val <valentinmichon2005@gmail.com>
2026-04-27 10:23:15 +0200

    Ajout retrofit et navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.7")
 }

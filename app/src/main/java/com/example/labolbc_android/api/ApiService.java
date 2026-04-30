39c28ce76f017e34b50306ff1ecfb7eb752dc4e6
Val <valentinmichon2005@gmail.com>
2026-04-30 10:16:44 +0200

    Ajout routes

--- app/src/main/java/com/example/labolbc_android/api/ApiService.java
+++ app/src/main/java/com/example/labolbc_android/api/ApiService.java
@@ -6,10 +6,34 @@ import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiService {
-    @POST("auth/login")
-    Call<LoginResponse> login(@Body LoginRequest request);
+    @GET("/visites")
+    Call<List<Visite>> getVisites();
 
+    @GET("/visites/{id}")
+    Call<Visite> getVisite(@Path("id") int id);
+
+    @GET("/visiteur/visites")
+    Call<List<Visite>> getVisitesVisiteur();
+
+    @GET("/visiteur/visites/{id}")
+    Call<Visite> getVisiteVisiteur(@Path("id") int id);
+
+    @POST("/visiteur/visites")
+    Call<List<Visite>> createVisite(@Body Visite visite);
+
+    @GET("/visiteur/visites/{id}/report")
+    Call<List<Visite>> createCompteRendu(@Path("id") int id, @Body Visite visite);
+
+    @PUT("/visiteur/visites/{id}")
+    Call<List<Visite>> updateVisite(@Path("id") int id, @Body Visite visite);
+
+    @DELETE("/visiteur/visites/{id}")
+    Call<List<Visite>> deleteVisite(@Path("id") int id);
+
+    @POST("/login")
+    Call<LoginResponse> login(@Body LoginRequest request);
 }
